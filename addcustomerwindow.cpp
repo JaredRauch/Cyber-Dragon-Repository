dbe443e86a596aa94de15acb7ad16b4774254155
jweiser2 <jweiser2@saddleback.edu>
2016-05-15 09:42:01 -0700

    minor errors fixed, try/catch commented out

--- addcustomerwindow.cpp
+++ addcustomerwindow.cpp
@@ -25,9 +25,9 @@ void AddCustomerWindow::on_rejectButton_clicked()
 
 void AddCustomerWindow::on_acceptButton_clicked()
 {
-    try{
+    //try{
 
-        int inter;
+        Interest inter;
 
         bool isKey;
 
@@ -37,7 +37,8 @@ void AddCustomerWindow::on_acceptButton_clicked()
                 (ui->ZipLineEdit->text() == "") ||
                 (ui->not_interested_radio->checkState() == Qt::Unchecked
                  && ui->some_interested_radio->checkState() == Qt::Unchecked &&
-                 ui->very_interested_radio->checkState() == Qt::Unchecked)
+                 ui->very_interested_radio->checkState() == Qt::Unchecked))
+
         {
             ErrorWindow *error = new ErrorWindow(0);
 
@@ -77,15 +78,15 @@ void AddCustomerWindow::on_acceptButton_clicked()
 
             if(ui->not_interested_radio->checkState() == Qt::Checked)
             {
-                inter = NOT_INTERESTED
+                inter = NOT_INTERESTED;
             }
             else if(ui->some_interested_radio->checkState() == Qt::Checked)
             {
-                inter = SOMEWHAT_INTERESTED
+                inter = SOMEWHAT_INTERESTED;
             }
             else if(ui->very_interested_radio->checkState() == Qt::Checked)
             {
-                inter = VERY_INTERESTED
+                inter = VERY_INTERESTED;
             }
 
             if(ui->IsKey_CheckBox->checkState() == Qt::Unchecked)
@@ -97,17 +98,20 @@ void AddCustomerWindow::on_acceptButton_clicked()
                 isKey = true;
             }
 
+
+
             db->AddCustomer(ui->NameLineEdit->text(), ui->StreetLineEdit->text(),
                             ui->CityLineEdit->text(), ui->statecomboBox->currentText(),
                             ui->ZipLineEdit->text(), inter, isKey);
 
         }
 
-    };
+    /*};
     catch()
     {
 
-    }
+    }*/
+}
 
 void AddCustomerWindow::on_not_interested_radio_clicked()
 {
@@ -130,3 +134,5 @@ void AddCustomerWindow::on_very_interested_radio_clicked()
 
     ui->some_interested_radio->setCheckState(Qt::Unchecked);
 }
+
+
